5094ed51e1b44db5d3dd6d89ac94d21365632bc5
Paul Hendriks <106840196+paulhendriks14@users.noreply.github.com>
2022-06-16 23:42:30 +0200

    Update streamlit_app.py
    indended button

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,10 +54,10 @@ def get_fruit_load_list():
             return my_cur.fetchall()
 
 # add a button to load the fruit
-if streamlit.button('Get Fruit Load List')
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_data_rows = get_fruit_load_list()
-streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
