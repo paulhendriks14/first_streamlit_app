9e7dfd2497d4c3f9342f3b9cd3efe7ffd02f4d5a
Paul Hendriks <106840196+paulhendriks14@users.noreply.github.com>
2022-06-16 22:45:56 +0200

    Update streamlit_app.py
    adding extra fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
+
+#New Section to display fruityvice api response
+add_fruit_choice = streamlit.text_input('What Fruit would you like to add?' , 'jackfruit')
+streamlit.write('Thanks for adding ', add_fruit_choice)
