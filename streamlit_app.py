3976c07bbbb6d89d4e99c589c2b4edcbf2457855
Paul Hendriks <106840196+paulhendriks14@users.noreply.github.com>
2022-06-16 08:40:00 +0200

    Update streamlit_app.py
    add resquests from fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = request.get("http://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("http://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
                             
 
